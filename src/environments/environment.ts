63f0e7531248678f727bed91934355fe1862c777
Juanita Mejia <juamejiapi@gmail.com>
2025-07-27 16:13:41 -0500

    sapa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  API_URL: 'https://website-deltamusic-backend-production.up.railway.app'  // URL backend Railway para pruebas
+  API_URL: 'https://website-delta-music-backend-production.up.railway.app'  // URL backend Railway para pruebas
 };
